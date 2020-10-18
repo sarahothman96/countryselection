7418600a32032332b6d5b033cfab941479462861
Sarah Othman <drsarah.othman@gmail.com>
2020-10-18 22:41:12 +0800

    Add Country Page

---
+++ src/views/Country.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <b-row class="d-flex justify-content-center">
+        <b-col cols="3">
+            <b-form-select v-model="selectedCountry" :options="countries" value-field="alpha3Code" text-field="name">
+            </b-form-select>
+        </b-col>
+    </b-row>
+
+    <b-row class="mt-5 d-flex justify-content-center" v-if="selectedCountry === 'MYS'">
+        Malaysia Ku Tanah Air Ku
+
+    </b-row>
+
+    <b-row class="mt-5 d-flex justify-content-center">
+        {{ selectedCountry }}
+    </b-row>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            countries: [],
+            selectedCountry: '',
+        }
+    },
+
+    created() {
+        this.loadAllCountries()
+    },
+
+    methods: {
+
+        loadAllCountries() {
+            axios.get('https://restcountries.eu/rest/v2/all')
+                .then(respond => {
+                    console.log(respond)
+                    this.countries = respond.data
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    },
+}
+</script>
+
+<style scope>
+
+</style>
